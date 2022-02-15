71480dd1a0940d11b4975ec86bdbbe9804dbbdf2
soylent <1593860+soylent@users.noreply.github.com>
2022-02-14 20:41:50 -0700

    Deploy to Polygon testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,19 @@
 require("@nomiclabs/hardhat-ethers");
 
+const fs = require("fs");
+const privateKey = fs
+  .readFileSync(".secret")
+  .toString()
+  .trim();
+
 module.exports = {
+  defaultNetwork: "matic",
+  networks: {
+    hardhat: {},
+    matic: {
+      url: "https://rpc-mumbai.maticvigil.com/v1/7a72ff82afc9b4434a6796571d5c38cdf0f8a853",
+      accounts: [privateKey]
+    }
+  },
   solidity: "0.8.10"
 };
