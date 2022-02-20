66f15930eb5dffd22c1970f1a971643b94c04444
soylent <1593860+soylent@users.noreply.github.com>
2022-02-20 00:01:40 -0700

    Add build command

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,18 +1,17 @@
 require("@nomiclabs/hardhat-ethers");
 
-const fs = require("fs");
-const privateKey = fs
-  .readFileSync(".secret")
-  .toString()
-  .trim();
-
+let accounts = [];
+if (process.env.PRIVATE_KEY) {
+  accounts.push(process.env.PRIVATE_KEY);
+}
 module.exports = {
   defaultNetwork: "hardhat",
   networks: {
     hardhat: {},
     matic: {
-      url: "https://rpc-mumbai.maticvigil.com/v1/7a72ff82afc9b4434a6796571d5c38cdf0f8a853",
-      accounts: [privateKey]
+      url:
+        "https://rpc-mumbai.maticvigil.com/v1/7a72ff82afc9b4434a6796571d5c38cdf0f8a853",
+      accounts: accounts
     }
   },
   solidity: "0.8.9"
