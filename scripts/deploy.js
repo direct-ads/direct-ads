af0cb91370628ce1d607bc6d3b1e822f73573a37
soylent <1593860+soylent@users.noreply.github.com>
2022-03-20 23:55:50 -0600

    Update seed data

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,26 +5,35 @@ async function main() {
   console.log("DirectAds deployed to:", directAds.address);
   console.log(`https://mumbai.polygonscan.com/address/${directAds.address}`);
 
-  // Add some test data for demo purposes
-  for (let i = 1; i < 5; i++) {
-    await directAds.addInventory(
-      "https://ipfs.infura.io/ipfs/QmcZfEzDizzLpSP2erfFXjfbgLhomWxStfjq9xtmza9cRr"
-    );
+  const tokens = [
+    "https://ipfs.infura.io/ipfs/QmRmkCerpVgwA6RTjA7LhAGBN8XSVWxHsr3b5vZ9AHXWTi",
+    "https://ipfs.infura.io/ipfs/QmRdCvssFGpLNbWCY7PJhP3d2rLXFjS9dzEb2sxSyzWhMk",
+    "https://ipfs.infura.io/ipfs/QmcGS1MkjTT1r5pfV1TWaC9Lg1qvWino6yz3orobou5mXx",
+    "https://ipfs.infura.io/ipfs/QmSmtgsyMv57jzNxZobb2RhMwQgHMcFPpEVymd7sUrrW73",
+  ];
+
+  // Add some test data
+  for (let i = 0; i < tokens.length; i++) {
+    const tokenURI = tokens[i];
+    console.log("adding", tokenURI);
+    await directAds._addInventory(tokenURI);
     for (let j = 0; j < 2; j++) {
+      let bid = Math.floor(Math.random() * 1000);
       await directAds.addOffer(
-        i,
-        "https://example.org/vast.xml",
-        500,
+        i+1,
+        `https://example${j+1}.org/vast${bid}.xml`,
+        bid,
         "0x5FbDB2315678afecb367f032d93F642f64180aa3"
       );
     }
   }
   try {
     await transferLinkTokens("0.05", directAds.address);
-    await directAds.startDomainVerification("example.com");
-  } catch(e) {
+    await directAds.startDomainVerification("example.com", tokenURI);
+  } catch (e) {
     console.error(e);
   }
+
   console.log("Seeding complete");
 }
 
